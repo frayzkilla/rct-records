054c1f2d809d33dd14c5e616269e1b10ab56a94e
frayzkilla <frayzthamotion@gmail.com>
2025-05-16 17:33:19 +0800

    updated about page and added styles for scroll-bar

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -5,25 +5,36 @@ export default function AboutPage() {
         О нас
       </h2>
 
-      <div className="max-w-4xl mx-auto text-center space-y-10 text-lg leading-relaxed text-zinc-300">
+      <div className="max-w-4xl mx-auto text-justify space-y-10 text-lg leading-relaxed text-zinc-300">
+        <div className="absolute top-[42%] right-6  text-[20rem] font-extrabold text-zinc-800 opacity-50 pointer-events-none select-none leading-none">
+          RAW   
+        </div>
         <p className="font-mono tracking-wide">
-          <span className="text-[#D4AF37] font-bold">RAW CROWNZ</span> — творческое объединение, где рождаются идеи
-          и создается стиль.
+          <span className="text-[#D4AF37] font-bold">RAW CROWNZ</span> —
+          творческое объединение, где рождаются идеи и создается стиль.
         </p>
 
         <p className="font-mono tracking-wide">
-          <span className="text-[#C9A227] font-bold">RAW CROWNZ <span className="underline">RECORDS</span></span>{" "}
+          <span className="text-[#C9A227] font-bold">
+            RAW CROWNZ <span className="underline">RECORDS</span>
+          </span>{" "}
           — одно из наших направлений, где мы растём, экспериментируем,
-          открываем новые стили и делаем музыку, в которой слышен город, мечты и амбиции.
+          открываем новые стили и делаем музыку, в которой слышен город, мечты и
+          амбиции.
         </p>
 
         <p className="font-mono tracking-wide">
           Мы верим в искренность, звук с характером и артистов, которые не
-          боятся быть собой. У нас нет фальши — только неподдельный интерес к тому, что будет завтра.
+          боятся быть собой. У нас нет фальши — только неподдельный интерес к
+          тому, что будет завтра.
         </p>
 
         <p className="font-mono tracking-wide">
-          Мы — те, кто делает по-своему. Мы — <span className="text-[#C9A227] animate-pulse font-extrabold">RAW</span>.
+          Мы — те, кто делает по-своему. Мы —{" "}
+          <span className="text-[#C9A227] animate-pulse font-extrabold">
+            RAW
+          </span>
+          .
         </p>
       </div>
     </div>
