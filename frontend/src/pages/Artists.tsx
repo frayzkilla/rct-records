6154ebde76642db3abca13418aa57312f82948a3
frayzkilla <frayzthamotion@gmail.com>
2025-05-18 02:55:14 +0800

    loading artists from backend

--- frontend/src/pages/Artists.tsx
+++ frontend/src/pages/Artists.tsx
@@ -1,24 +1,23 @@
+import { useEffect, useState } from "react";
 import ArtistCard from "../components/ArtistCard";
 
-const artists = [
-  {
-    name: "Leftover",
-    description: "битмейкер, эмси, звукорежиссёр",
-    image: "/public/artists_images/leftover.jpg",
-  },
-  {
-    name: "Frayz The Raw",
-    description: "продюсер, плэйбой, аристократ",
-    image: "/public/artists_images/frayztheraw.jpg",
-  },
-  {
-    name: "Danny Local",
-    description: "битмейкер, эмси, лысый из браззерс",
-    image: "/public/artists_images/dannylocal.jpg",
-  },
-];
+type Artist = {
+  id: string;
+  name: string;
+  bio: string;
+  avatarUrl: string;
+};
 
 export default function ArtistsPage() {
+  const [artists, setArtists] = useState<Artist[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/artists")
+      .then((res) => res.json())
+      .then(setArtists)
+      .catch((err) => console.error("Ошибка при загрузке артистов", err));
+  }, []);
+
   return (
     <div className=" bg-black text-white font-sans px-6 py-24 min-h-screen">
       <h2 className="text-6xl md:text-7xl text-center text-transparent font-bold font-mono uppercase tracking-widest bg-clip-text bg-gradient-to-r from-[#C9A227] via-[#D4AF37] to-[#E0C068] mb-16">
@@ -27,10 +26,10 @@ export default function ArtistsPage() {
       <div className="flex flex-wrap justify-center gap-10">
         {artists.map((artist) => (
           <ArtistCard
-            key={artist.name}
+            key={artist.id}
             name={artist.name}
-            description={artist.description}
-            image={artist.image}
+            description={artist.bio}
+            image={`http://localhost:3000/${artist.avatarUrl}`}
           />
         ))}
       </div>
