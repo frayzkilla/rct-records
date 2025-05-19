f397c87196c9f765f2029ce8939bb1dd3deefe76
frayzkilla <frayzthamotion@gmail.com>
2025-05-20 03:08:31 +0800

    added loading beats from backend

--- frontend/src/pages/Beats.tsx
+++ frontend/src/pages/Beats.tsx
@@ -1,27 +1,24 @@
 import BitCard from "../components/TrackCard";
+import { useEffect, useState } from "react";
 
-const beats = [
-  {
-    title: "Simon Said",
-    producer: "Frayz The Raw",
-    cover: "/public/covers/frayztheraw_simonsaid.jpg",
-    audio: "/frayztheraw_simonsaid.mp3",
-  },
-  {
-    title: "Woah",
-    producer: "Leftover",
-    cover: "/public/covers/leftover_woah.jpg",
-    audio: "/leftover_woah.mp3",
-  },
-  {
-    title: "Van Two",
-    producer: "Danny Local",
-    cover: "/public/covers/dannylocal_vantwo.jpg",
-    audio: "/dannylocal_vantwo.mp3",
-  },
-];
+type Beat = {
+  id: string;
+  title: string;
+  audioUrl: string;
+  coverUrl: string;
+  producer: string;
+};
 
 export default function BeatsPage() {
+  const [beats, setBeats] = useState<Beat[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/beats")
+      .then((res) => res.json())
+      .then(setBeats)
+      .catch((err) => console.error("Ошибка при загрузке битов", err));
+  }, []);
+
   return (
     <div className="bg-black text-white font-sans px-6 py-24 min-h-screen">
       <h2 className="text-6xl md:text-7xl text-center text-transparent font-bold font-mono uppercase tracking-widest bg-clip-text bg-gradient-to-r from-[#C9A227] via-[#D4AF37] to-[#E0C068] mb-16">
@@ -30,11 +27,11 @@ export default function BeatsPage() {
       <div className="flex flex-wrap justify-center gap-10">
         {beats.map((beat) => (
           <BitCard
-            key={beat.title}
+            key={beat.id}
             title={beat.title}
             producer={beat.producer}
-            cover={beat.cover}
-            audio={beat.audio}
+            cover={beat.coverUrl}
+            audio={beat.audioUrl}
           />
         ))}
       </div>
